5a953c1ff609e7a97ef5d5c873edde0580625fd8
Joel Rieger <rieger@gmail.com>
2017-12-03 15:09:42 -0800

    Corrected Glitches in RRC.

--- filter_ir.py
+++ filter_ir.py
@@ -18,15 +18,33 @@ def root_raised_cos(Ns,b,M=6):
     import numpy as np
     from numpy import pi,sinc,cos,sin,sqrt
     
+    #s_arr=np.arange(-M*Ns,M*Ns+1)
     s_arr=np.linspace(-M*Ns,M*Ns,num=2*Ns*M+1)
     ir=np.zeros(len(s_arr))
+
     
     for i,t in enumerate(s_arr):
-        if t==0:
-            ir[i]=1/Ns*(1.0+b*(4/pi-1.0))
-        elif (1-(2*b*t/Ns)**2)==0:
-            ir[i]=b/(Ns*sqrt(2))*((1.0+2/pi)*sin(pi/(4*b))+(1.0-2.0/pi)*cos(pi/(4*b)))
-        else:
-            ir[i]=1.0/Ns*(sin(pi*t/Ns*(1.0-b))+4*b*t/Ns*cos(pi*t/Ns*(1.0+b)))/(pi*t/Ns*(1.0-(4*b*t/Ns)**2))
-
+       if abs(1 - 16*b**2*(t/Ns)**2) == 0:
+           ir[i] = 0.5*((1.0+b)*sin((1.0+b)*pi/(4.0*b))-(1.0-b)*cos((1.0-b)*pi/(4.0*b))+(4.0*b)/pi*sin((1.0-b)*pi/(4.0*b)))
+       else:
+           ir[i] = 4.0*b/(pi*(1.0-16.0*b**2*(t/Ns)**2))
+           ir[i] = ir[i]*(np.cos((1.0+b)*pi*t/Ns)+sinc((1.0-b)*t/Ns)*(1.0-b)*pi/(4.0*b))
+           
     return ir
+
+
+if __name__=='__main__':
+    from matplotlib import pyplot as plt
+    from digital_comm import *
+    
+    ir_rc=root_raised_cos(6.0,0.35)
+    plt.plot(ir_rc)
+
+    rrc_ir=sqrt_rc_imp(6.0,0.35)
+    plt.plot(rrc_ir)
+
+    #diff=[abs(a-b) for a,b in zip(ir_rc,rrc_ir)]
+    
+    plt.plot(ir_rc)
+    plt.plot(rrc_ir)
+    plt.show()
--- sampling.py
+++ sampling.py
@@ -36,19 +36,19 @@ if __name__=='__main__':
     from random import randint
 
     from filter_ir import *
-
+    
     sample_rate=32e3
-    samples_per_symbol=32
+    samples_per_symbol=8
     m=4 #constelation size
-    num_sym=16 #number of symbols
+    num_sym=64 #number of symbols
 
     symbol_map={0:(-1.0,-1.0),1:(-1.0,1.0),2:(1.0,-1.0),3:(1.0,1.0)}
 
-    rrc_ir=root_raised_cos(samples_per_symbol,0.35,M=6)
-
+    rrc_ir=root_raised_cos(samples_per_symbol,0.35,M=6)    
+    
     data=np.array([randint(0,m-1) for tmp in range(num_sym)])
     IQ_n1=np.pad(np.array([symbol_map[x][0]+symbol_map[x][1]*1j for x in data]),10,mode='constant')
-
+    
     IQ_n2=upsample(IQ_n1,samples_per_symbol)
 
     IQ_n3=signal.convolve(IQ_n2,rrc_ir,mode='same')
